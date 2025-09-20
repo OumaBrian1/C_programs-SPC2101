995e8b402ede4e79db0b2fc6452c44dae3407047
OumaBrian1 <brianouma1461@gmail.com>
2025-09-20 15:17:31 -0700

    Add files via upload

---
+++ height,ID,balance enter .c
@@ -0,0 +1,21 @@
+//simple user prompt program displaying height,ID number and bank balance
+/*
+Name:Brian Ouma
+Reg no:PA106/G/28812/25
+Description:program to prompt user to enter height,ID number and bank balance
+*/
+#include<stdio.h>//preprocessor directive printf(),scanf();
+
+//main function
+int main() {
+	printf("1:Enter your height in metres \n");
+	scanf("%f,&height \n");
+	
+	printf("2:Enter your ID number \n");
+	scanf("%f,&ID no \n");
+	
+	printf("3:Enter bank balance \n");
+	scanf("%f,&bank balance \n");
+	
+	return 0;
+	}
