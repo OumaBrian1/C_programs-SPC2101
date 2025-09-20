8c963c95fded400072f762af6933eb5b75cdf4f5
OumaBrian1 <brianouma1461@gmail.com>
2025-09-20 14:25:32 -0700

    Add files via upload

---
+++ units_display.c
@@ -0,0 +1,21 @@
+//simple alll units for the semester program
+/*
+Name:Brian Ouma
+Reg no:PA106/G/28812/25
+Description:program to display all units for the first semester
+*/
+#include<stdio.h>//preprocessors directive:scanf(),printf()
+
+//main function
+int main() {
+	printf("Discrete Mathematics SPM2100!!\n");
+	printf("Mathematics for Science SPM2104!!\n");
+	printf("Fundamentals of ICT UCCU2105!!\n");
+	printf("Computer System and Organisation SPI2102!!\n");
+	printf("Communication Skills UCU2101!!\n");
+	printf("Introduction to Programming and Problem Solving SPC2101!!\n");
+	printf("Fundamental of System Analysis and Design SSE2101!!\n");
+	
+	return 0;
+	
+}
